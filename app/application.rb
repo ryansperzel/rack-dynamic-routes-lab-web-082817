21ddf43ecea39db4bf074b392bbe42b1ac422b6a
github username <github email address>
2017-09-18 11:38:22 -0400

    Done

---
+++ app/application.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+
+class Application
+
+  @@items = [Item.new('grapes', 3.22), Item.new('lettuce', 2.75)]
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split(/items/).last
+      if item =@@items.find{|i| "/" + i.name == item_name}
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+
+
+
+end
--- app/item.rb
+++ app/item.rb
@@ -1,6 +1,7 @@
 class Item
   attr_accessor :name, :price
 
+
   def initialize(name,price)
     @name = name
     @price = price
